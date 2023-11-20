31670ab8b1c3fd3b6a743a265894530b28b99122
skybondm <150842432+skybondm@users.noreply.github.com>
2023-11-20 17:51:04 +0300

    Add files via upload

--- shop.js
+++ shop.js
@@ -1,3 +1,16 @@
 function print_contact () {
     alert('skype: helper\ntelegram: @helper\ntel: 8995345235')
 }
+function basket () {
+    window.location.href="shop2.html"
+}
+function change_color () {
+    let a = document.getElementById ("color")
+    //проверка заднего фона
+    if ( window.getComputedStyle(a).backgroundColor == "rgb(255, 0, 0) ")
+    {
+        a.style.backgroundColor = "green";
+    }
+    else
+    a.style.backgroundColor = "red";
+}

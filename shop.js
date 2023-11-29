6cff4e6fe467cc3f2eea3d3a0331d2e77b0c2a35
skybondm <150842432+skybondm@users.noreply.github.com>
2023-11-29 16:10:26 +0300

    Add files via upload

--- shop.js
+++ shop.js
@@ -7,10 +7,48 @@ function basket () {
 function change_color () {
     let a = document.getElementById ("color")
     //проверка заднего фона
-    if ( window.getComputedStyle(a).backgroundColor == "rgb(255, 0, 0) ")
+    if ( window.getComputedStyle(a).backgroundColor == "rgb(255, 0, 0)")
     {
         a.style.backgroundColor = "green";
+        a.style.color = "black"
+        a.style.borderRadius = "0"
+        a.style.boxShadow = "5px 5px 10px green"
+        a.style.padding = "30px"
     }
     else
-    a.style.backgroundColor = "red";
+    {
+        a.style.backgroundColor = "red";
+        a.style.color = "white"
+        a.style.borderRadius = "5px"
+        a.style.boxShadow = "5px 5px 10px black"
+        a.style.padding = "5px"
+    }
+}
+
+function prove () {
+    let x = document.getElementById ("prove")
+    //проверка чисел
+    x = prompt('Докажите, что вы человек: 2+2*2')
+    y = 6
+    if (x==y)
+    {
+        alert('Добро пожаловать!')
+    }
+    else 
+    {
+        alert('Петляй отсюда, псина сутулая, тебе здесь не рады')
+        window.close ()
+    }
+}
+function modal () {
+    let a = document.getElementById ("modal")
+    //проверка свойства display
+    if ( window.getComputedStyle(a).display == "none")
+    {
+        a.style.display = "flex"
+    }
+    else
+    {
+        a.style.display = "none"
+    }
 }
